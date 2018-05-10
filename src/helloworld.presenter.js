1874875dd25f34b88ff0ebcd4006f425f4acc8cb
Matteo Pierro <pierromatteo@gmail.com>
2018-05-10 12:40:13 +0200

    handle empty name

--- src/helloworld.presenter.js
+++ src/helloworld.presenter.js
@@ -1,5 +1,9 @@
 const HelloWorldPresenter = function (view) {
     this.greetings = function (name) {
+        if (name === undefined || name === '') {
+            view.showMessage("Hello World");
+            return;
+        }
         view.showMessage("Hello " + name);
     }
 }
--- test/helloworld.presenter.test.js
+++ test/helloworld.presenter.test.js
@@ -4,13 +4,24 @@ const HelloWorldPresenter = require('../src/helloworld.presenter').HelloWorldPre
 const HelloWorldView = require('../src/helloworld.view').HelloWorldView;
 
 describe('HelloWorldPresenter', function () {
-    it('should greet with a name when present', function () {
+    it('should greet with a name when specified', function () {
         let view = new HelloWorldView();
         let presenter = new HelloWorldPresenter(view);
 
         let viewMock = sinon.mock(view);
         viewMock.expects('showMessage').withArgs('Hello pluto');
         presenter.greetings('pluto');
+
+        viewMock.verify();
+    });
+
+    it('should greet the world then a name is not specified', function () {
+        let view = new HelloWorldView();
+        let presenter = new HelloWorldPresenter(view);
+
+        let viewMock = sinon.mock(view);
+        viewMock.expects('showMessage').withArgs('Hello World');
+        presenter.greetings();
 
         viewMock.verify();
     });
